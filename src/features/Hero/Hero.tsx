8ed8ba68e1d116e2ae1e5a1aec81b20622a1306a
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-24 14:45:39 +0300

    made a basic aboutsection

--- src/features/Hero/Hero.tsx
+++ src/features/Hero/Hero.tsx
@@ -3,24 +3,29 @@
 import { motion } from "framer-motion";
 import { HeroStyled } from "./Styled";
 import { optAnimation, piecesIntro } from "./uiFactory";
+import About from "./components/About";
 
 const Hero = () => {
   return (
-    <HeroStyled className="w-full flex flex-col gap-3 sm:gap-6">
-      {piecesIntro.map((el, i) => (
-        <motion.div
-          key={el.id}
-          custom={i}
-          variants={optAnimation}
-          initial="hidden"
-          animate="visible"
-          // {...(i % 2 === 0 ? anmRight : anmLeft)}
+    <HeroStyled className="w-full flex flex-col gap-8 sm:gap-12">
+      <div className="w-full flex flex-col gap-3 sm:gap-5">
+        {piecesIntro.map((el, i) => (
+          <motion.div
+            key={el.id}
+            custom={i}
+            variants={optAnimation}
+            initial="hidden"
+            animate="visible"
+            // {...(i % 2 === 0 ? anmRight : anmLeft)}
 
-          className="w-full flex justify-center"
-        >
-          <span className={`grad_txt ${el.fnt}`}>{el.txt}</span>
-        </motion.div>
-      ))}
+            className="w-full flex justify-center"
+          >
+            <span className={`grad_txt ${el.fnt}`}>{el.txt}</span>
+          </motion.div>
+        ))}
+      </div>
+
+      <About />
     </HeroStyled>
   );
 };
---
+++ src/features/Hero/components/About.tsx
@@ -0,0 +1,57 @@
+/** @jsxImportSource @emotion/react */
+
+"use client";
+
+import { easeInOut, motion } from "framer-motion";
+import { type FC } from "react";
+import { css } from "@emotion/react";
+
+const About: FC = () => {
+  return (
+    <motion.div
+      className="w-full max-w-[600px]  mx-auto"
+      style={{ willChange: "transform, opacity" }}
+      initial={{
+        y: 200,
+        opacity: 0,
+      }}
+      transition={{
+        duration: 0.5,
+        delay: 0.6,
+        ease: easeInOut,
+      }}
+      animate={{
+        opacity: 1,
+        y: 0,
+      }}
+    >
+      <div
+        className="rounded-3xl p-5 "
+        css={css`
+          border: 2px solid var(--whitesmoke);
+          transition: 0.3s;
+
+          span {
+            color: var(--whitesmoke);
+          }
+
+          &:hover {
+            box-shadow: 0 0 10px var(--whitesmoke), 0 0 20px var(--whitesmoke),
+              0 0 30px var(--whitesmoke);
+          }
+        `}
+      >
+        <span className="txt__md">
+          I&apos;m always exploring new tools and frameworks and applying them
+          in&nbsp;
+          <strong>real-world projects.</strong> I care about building&nbsp;
+          <strong>scalable, maintainable apps</strong>
+          with clean code and&nbsp;
+          <strong>solid architecture.</strong>
+        </span>
+      </div>
+    </motion.div>
+  );
+};
+
+export default About;
