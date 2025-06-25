95cefc62e91de4f307a4049ef63b3872c240c4de
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-25 13:14:34 +0300

    fix type props to nt fail build

--- src/features/Projects/components/FilterBtn/FilterBtn.tsx
+++ src/features/Projects/components/FilterBtn/FilterBtn.tsx
@@ -1,18 +1,10 @@
-'use client';
+"use client";
 
-import type { FC } from 'react';
-import { FilterBtnStyled } from './Styled';
+import type { FC } from "react";
+import { FilterBtnStyled } from "./Styled";
 
-type PropsType = {
-  
-};
-
-const FilterBtn: FC<PropsType> = ({ }) => {
-  return (
-    <FilterBtnStyled>
-	
-    </FilterBtnStyled>
-  );
+const FilterBtn: FC = ({}) => {
+  return <FilterBtnStyled></FilterBtnStyled>;
 };
 
 export default FilterBtn;
