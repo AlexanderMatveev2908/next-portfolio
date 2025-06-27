334552a676bc458120304ac1e6ff1b7b478cdc4d
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-27 17:52:29 +0300

    added animation btn contact , just first part, now second

--- src/core/layout/components/Header/Header.tsx
+++ src/core/layout/components/Header/Header.tsx
@@ -1,17 +1,34 @@
+/** @jsxImportSource @emotion/react */
 "use client";
 
 import type { FC } from "react";
 import { HeaderStyled } from "./Styled";
 import HeaderLink from "@/core/layout/components/Header/components/HeaderLink/HeaderLink";
 import { headerLinks } from "./uiFactory";
+import { css } from "@emotion/react";
 
 const Header: FC = ({}) => {
   return (
     <HeaderStyled className="w-full grid grid-cols-[1fr_2fr] sticky top-0 z-60 pt-[15px] sm:pt-[25px] pb-[20px] pad_app">
       <div className="w-full flex justify-start ">
-        <HeaderLink
-          {...{ $clr: "gray__300", txt: "A.M — Full-Stack", to: "hero" }}
-        />
+        <div
+          onClick={() => {
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            });
+          }}
+          className="link cursor-pointer"
+        >
+          <span
+            css={css`
+              color: var(--gray__300);
+            `}
+            className="txt__lg font-semibold"
+          >
+            A.M — Full-Stack
+          </span>
+        </div>
       </div>
 
       <div className="w-full flex gap-5 sm:gap-10 justify-end items-start">
--- src/features/Contact/components/FancyBtnV2/FancyBtnV2.tsx
+++ src/features/Contact/components/FancyBtnV2/FancyBtnV2.tsx
@@ -7,6 +7,7 @@ import Anchor from "../Anchor/Anchor";
 import { css } from "@emotion/react";
 import { btnFancySVGs } from "./uiFactory";
 import { genSizeRandom } from "@/core/lib/etc";
+import { easeInOut, motion } from "framer-motion";
 
 type PropsType = {
   href: string;
@@ -17,28 +18,46 @@ const FancyBtnV2: FC<PropsType> = ({ href }) => {
 
   return (
     <Anchor {...{ href, setIsHover }}>
-      <div
+      <motion.div
         className="w-full flex justify-center max-w-fit h-full max-h-fit relative"
         css={css`
           border: 2px solid var(--whitesmoke);
           padding: 7.5px 50px;
           border-radius: 15px;
         `}
+        transition={{
+          duration: 0.4,
+          ease: easeInOut,
+        }}
+        animate={{
+          transform: isHover
+            ? "scale(1.2) rotate(-10deg)"
+            : "scale(1) rotate(0deg)",
+        }}
       >
-        <div
-          className="absolute min-w-full min-h-full top-[0.75rem] -left-[1rem] border-2"
+        <motion.div
+          className="absolute min-w-full min-h-full top-0 left-0 border-2"
           css={css`
             border-radius: 15px;
             border: 2px solid var(--whitesmoke);
           `}
-        ></div>
+          transition={{
+            duration: 0.4,
+            ease: easeInOut,
+          }}
+          animate={{
+            transform: isHover
+              ? "translateY(0.75rem) translateX(-1rem)"
+              : "scale(1) rotate(0deg)",
+          }}
+        ></motion.div>
 
         <div className="absolute inset-0 z-40 bg-neutral-950 rounded-[15px]"></div>
 
         <span className="txt__lg font-bold text-[whitesmoke] relative z-60">
           Contact
         </span>
-      </div>
+      </motion.div>
 
       {btnFancySVGs.map((el) => (
         <el.svg
