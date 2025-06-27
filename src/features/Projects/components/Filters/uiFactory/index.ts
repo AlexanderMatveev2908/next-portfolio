a8c1d0f47c739c5e9fe1d9b133f129ac9a5234cd
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-27 08:57:31 +0300

    added opt filter

--- src/features/Projects/components/Filters/uiFactory/index.ts
+++ src/features/Projects/components/Filters/uiFactory/index.ts
@@ -2,7 +2,7 @@ import { FilterAppType } from "@/features/Projects/slice";
 import { css } from "@emotion/react";
 import { v4 } from "uuid";
 
-export const btnFilters = ["All", "Full-Stack", "Front-Only"].map((el) => ({
+export const btnFilters = ["All", "Full-Stack", "Frontend-Only"].map((el) => ({
   label: el,
   id: v4(),
 })) as { label: FilterAppType; id: string }[];
