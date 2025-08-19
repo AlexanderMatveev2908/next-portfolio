56d7bef1a1707037f085807fe4e4456299848dcf
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-19 10:39:33 +0300

    main => _

--- src/shared/components/HOC/WrapSection/WrapSection.tsx
+++ src/shared/components/HOC/WrapSection/WrapSection.tsx
@@ -1,8 +1,11 @@
+/** @jsxImportSource @emotion/react */
+
 "use client";
 
 import type { FC, SVGProps } from "react";
 import Txt from "../../elements/Txt/Txt";
 import { motion } from "framer-motion";
+import { css, SerializedStyles } from "@emotion/react";
 
 type PropsType = {
   el: {
@@ -12,9 +15,10 @@ type PropsType = {
   };
   children: React.ReactNode;
   id?: string;
+  $CSS?: SerializedStyles;
 };
 
-const WrapSection: FC<PropsType> = ({ el, children, id }) => {
+const WrapSection: FC<PropsType> = ({ el, children, id, $CSS }) => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -23,6 +27,9 @@ const WrapSection: FC<PropsType> = ({ el, children, id }) => {
       animate={{ opacity: 1 }}
       id={id ?? undefined}
       className="w-full flex flex-col justify-center gap-10"
+      css={css`
+        ${$CSS}
+      `}
     >
       <div className="w-full flex justify-start gap-6 items-center">
         <el.svg className="w-[30px] sm:w-[45px] h-[30px] sm:h-[45px] text-[var(--whitesmoke)]" />
